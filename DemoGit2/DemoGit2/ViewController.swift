c0cf39bcb36e378d3e0e605d13eaf058046f9912
HaiPV <phamhaikb942@gmail.com>
2021-01-12 17:29:09 +0700

    first init

---
+++ DemoGit2/DemoGit2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoGit2
+//
+//  Created by minerva on 1/12/21.
+//  Copyright © 2021 minerva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
